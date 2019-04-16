69d2fc3c97fdc5f8dfb90617ef295a77dc9b5012
suremotoo <suremotoo@163.com>
2019-04-16 18:05:28 +0800

    detail list recursive

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="content"></div>
   </div>
 </template>
@@ -9,12 +10,53 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人五人联票',
+          children: [{
+            title: '成人五人联票 - 某一连锁酒店销售'
+          }]
+        }, {
+          title: '成人三人联票',
+          children: [{
+            title: '成人三人联票 - 某一连锁酒店销售'
+          }]
+        }]
+      }, {
+        title: '学生票',
+        children: [{
+          title: '学生五人联票'
+        }, {
+          title: '学生三人联票'
+        }]
+      }, {
+        title: '儿童票',
+        children: [{
+          title: '儿童五人联票'
+        }, {
+          title: '儿童三人联票'
+        }]
+      }, {
+        title: '优惠票',
+        children: [{
+          title: '优惠五人联票'
+        }, {
+          title: '优惠三人联票'
+        }]
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="item" v-for="(item, index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <div class="item-title-icon"></div>
+        {{item.title}}
+      </div>
+      <div class="item-child" v-if="item.children">
+        <detail-list :list="item.children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    list: Array
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .item-title
+    line-height: .8rem
+    font-size: .32rem
+    padding: 0 .2rem
+    .item-title-icon
+      position: relative
+      display: inline-block;
+      width: .36rem
+      height: .36rem
+      top: .06rem
+      left: .06rem
+      background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+      margin-right: .1rem
+      background-size: .4rem 3rem
+  .item-child
+    padding: 0 .2rem
+</style>
